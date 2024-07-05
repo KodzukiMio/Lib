37a8a8c592cabe4892653237abb1267dd44fb4db
KodzukiMio <87706365+KodzukiMio@users.noreply.github.com>
2024-07-05 18:24:39 +0800

    Add files via upload

--- KurLibrary.cs
+++ KurLibrary.cs
@@ -13,96 +13,100 @@ using System.Threading;
 using System.Web;
 using System.Security.Cryptography;
 
-namespace KUR
+namespace KUR {
+    public delegate dynamic ActionOrFunc(dynamic args);
+    public delegate TResult ActionOrFunc<TResult>(params object[] args);
+    /*ActionOrFunc myFunc = (args) =>
 {
-    public class V8 : BaseClass<V8>
-    {
+    foreach (var arg in args) {
+        ...
+    }
+    return ...
+};
+*/
+    public class V8 : BaseClass<V8> {
         public bool input_flag = true;
-        public V8ScriptEngine engine = null;
-        public ScriptC scriptC = null;
-        public V8()
-        {
+        public V8ScriptEngine engine;
+        public ScriptC scriptC;
+        public bool net = false;
+        public V8() {
             this.engine = new V8ScriptEngine();
             this.scriptC = new ScriptC(this.engine, this);
             Load();
         }
-        public void Load()
-        {
+        public void Load() {
             engine.AddHostObject("SYS", this.scriptC);
             engine.AddHostType(typeof(JsonConvert));
             engine.AddHostObject("LIB", new Microsoft.ClearScript.HostTypeCollection("mscorlib", "System.Core"));
             engine.Execute(@"const Cout =function (value) {if (typeof value === 'function') {value = value.toString(); } else {value = SYS.SerializeObject(value);}SYS.WriteLine(value);};");
             engine.Execute(@"const _ = (obj) => {Cout(typeof(obj));let properties = new Set();let currentObj = obj;do {Object.getOwnPropertyNames(currentObj).map(item => properties.add(item));} while ((currentObj = Object.getPrototypeOf(currentObj)))return [...properties.keys()];}");
-            engine.Execute(@"const Console=LIB.System.Console;");
+            engine.Execute(@"const Console=LIB.System.Console;console.log=function(v){Console.WriteLine(v);};");
+            engine.Execute(@"let LOGTMP=[];console.log=function(v){LOGTMP.push(v);};function _OUT(){let str='';for(let i =0;i<LOGTMP.length;i++){str+=LOGTMP[i];str+='<br>';};LOGTMP=[];return str;}");
+        }
+        static string _tmp = @"_OUT()";
+        public string GetOutPut() {//net
+            return _Eval(ref _tmp).Replace("\"", "");
+        }
+        public void EnableNetwork() {
+            this.net = true;
+        }
+        public void PrintDynamicNames() {
             dynamic dynamicResult = engine.Evaluate("this");
             foreach (var name in dynamicResult.GetDynamicMemberNames())
                 Console.WriteLine("{0}: {1}", name, dynamicResult[name]);
         }
         public void SetInputFlag(bool _flag) { this.input_flag = _flag; }
-        public string Eval(ref string str)
-        {
-            try
-            {
+        public string Eval(ref string str) {//net or local
+            string ret = _Eval(ref str);
+            if (!this.net) return ret;
+            string retc = GetOutPut();
+            return retc.Length > 0 ? retc : ret;
+        }
+        public string _Eval(ref string str) {//local
+            try {
                 var result = engine.Evaluate(str);
-                if (result != null)
-                {
-                    if (result.ToString() == "[undefined]")
-                    {
-                        Console.WriteLine("[undefined]");
-                        return ("");
-                    };
+                if (result != null) {
+                    if (result.ToString() == "[undefined]") return ("");
                     var serializedResult = JsonConvert.SerializeObject(result);
                     return serializedResult;
                 }
-            } catch (Exception)
-            {
-                try
-                {
-                    Console.WriteLine(JsonConvert.SerializeObject(engine.Evaluate("_(" + str + ");")));
-                } catch (Exception ex_)
-                {
+            } catch (Exception) {
+                try {
+                    return JsonConvert.SerializeObject(engine.Evaluate("_(" + str + ");"));
+                } catch (Exception ex_) {
                     return ex_.Message;
                 }
             }
-            return ("____________");
+            return ("");
         }
-        public void InterActive(string Fmsg = ">>")
-        {
-            string input;
+        public void InterActive(string Fmsg = ">>") {
+            string? input;
             input_flag = true;
-            while (input_flag)
-            {
+            while (input_flag) {
                 Console.Write(Fmsg);
                 input = Console.ReadLine();
-                Console.WriteLine(Eval(ref input));
+                if (input != null) Console.WriteLine(_Eval(ref input));
             };
         }
 
     };
-    public class ScriptC
-    {
-        public V8ScriptEngine engine = null;
-        public V8 v8 = null;
-        public ScriptC(V8ScriptEngine _engine, V8 v8_)
-        {
+    public class ScriptC {
+        public V8ScriptEngine engine;
+        public V8 v8;
+        public ScriptC(V8ScriptEngine _engine, V8 v8_) {
             this.engine = _engine;
             this.v8 = v8_;
         }
-        public string SerializeObject(object value)
-        {
-            try
-            {
+        public string SerializeObject(object value) {
+            try {
                 return JsonConvert.SerializeObject(value);
-            } catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 return ex.Message;
             };
         }
-        public void Global()
-        {
+        public void Global() {
             var propertyNames = engine.Global.PropertyNames.GetEnumerator();
-            while (propertyNames.MoveNext())
-            {
+            while (propertyNames.MoveNext()) {
                 string element = propertyNames.Current;
                 string gettype = ("(typeof " + element + ")");
                 Console.WriteLine($"{v8.Eval(ref gettype)} :'{element}'");
@@ -110,31 +114,24 @@ namespace KUR
             propertyNames.Dispose();
         }
         public dynamic Window() { return engine.Script; }
-        public void WriteLine(string value)
-        {
+        public void WriteLine(string value) {
             Console.WriteLine(value);
         }
         public void EXIT() { v8.SetInputFlag(false); }
     }
-    public abstract class BaseClass<T> where T : BaseClass<T>, new()
-    {//静态创建
-        public static T Create(params object[] args)
-        {
+    public abstract class BaseClass<T> where T : BaseClass<T>, new() {//静态创建
+        public static T Create(params object[] args) {
             return (T)Activator.CreateInstance(typeof(T), args);
         }
-        public static T Create()
-        {
+        public static T Create() {
             return new T();
         }
     };
-    class Fst
-    {
+    class Fst {
         public static Action<string> Cout = System.Console.WriteLine;
         static public void PressContinue(string msg) { Fst.Cout(msg); Console.ReadKey(); }
-        public static bool SendEmail(string reciveAddress, string Subject, string body, string from = "Kurzer@foxmail.com", string key = "umpolxknqzdrbadi", string client = "smtp.qq.com", int port = 587, bool ssl = true)
-        {
-            try
-            {
+        public static bool SendEmail(string reciveAddress, string Subject, string body, string from = "Kurzer@foxmail.com", string key = "umpolxknqzdrbadi", string client = "smtp.qq.com", int port = 587, bool ssl = true) {
+            try {
                 MailMessage mail = new MailMessage();
                 mail.From = new MailAddress(from);
                 mail.To.Add(reciveAddress);
@@ -146,250 +143,37 @@ namespace KUR
                 smtp.EnableSsl = ssl;
                 smtp.Send(mail);
                 return true;
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.WriteLine(e.ToString());
                 return false;
             }
         }
     }
 
-    delegate dynamic ActionOrFunc(dynamic args);
-    delegate TResult ActionOrFunc<TResult>(params object[] args);
-    /*ActionOrFunc myFunc = (args) =>
-    {
-        foreach (var arg in args) {
-            ...
-        }
-        return ...
-    };
-
-    // 调用方法
-    string result = (string)myFunc(123, "abc", DateTime.Now);
-    Console.WriteLine(result);*/
-    public delegate void SocketHandle(StreamWriter writer_, StreamReader reader_, Server server);
-
-
-    public class Server
-    {
-        private TcpListener server;
-        private Boolean isRunning;
-        public SocketHandle sh;
-        public dynamic outSource;
-        public Server(int port, KUR.SocketHandle sh_)
-        {
-            server = new TcpListener(IPAddress.Any, port);
-            server.Start();
-            isRunning = true;
-            this.sh = sh_;
-        }
-
-        public void StartLoopClients()
-        {
-            while (isRunning)
-            {
-                TcpClient newClient = server.AcceptTcpClient();
-
-                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
-                t.Start(newClient);
-            }
-        }
-
-        public void HandleClient(object obj)
-        {
-            TcpClient client = (TcpClient)obj;
-
-            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
-            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
-
-            Boolean bClientConnected = true;
-
-            while (bClientConnected)
-            {
-                try
-                {
-                    if (sReader.BaseStream.CanRead) // 检查StreamReader是否已经关闭
-                    {
-                        sh(sWriter, sReader, this);
-                    } else
-                    {
-                        bClientConnected = false;
-                    }
-                } catch (IOException)
-                {
-                    bClientConnected = false;
-                    sWriter.Close();
-                    sReader.Close();
-                    client.Close();
-                } catch (ObjectDisposedException)
-                {
-                    bClientConnected = false;
-                    sWriter.Close();
-                    sReader.Close();
-                    client.Close();
-                }
-            }
-        }
-
-        public void Send(StreamWriter sWriter, string data)
-        {
-            sWriter.WriteLine(data);
-            sWriter.Flush();
-        }
-
-        public string Receive(StreamReader sReader)
-        {
-            string message = sReader.ReadLine();
-            return message;
-        }
-    }
-    public class Client
-    {
-        private TcpClient client;
-        private StreamWriter sWriter;
-        private StreamReader sReader;
-        public Client(string ipAddress, int port)
-        {
-            client = new TcpClient();
-            client.Connect(ipAddress, port);
-            sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
-            sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
-        }
-
-        public void Send(string data)
-        {
-            try
-            {
-                sWriter.WriteLine(data);
-                sWriter.Flush();
-            } catch (IOException)
-            {
-                sWriter.Close();
-                sReader.Close();
-                client.Close();
-            }
-        }
-
-        public string Receive()
-        {
-            string message = null;
-            try
-            {
-                message = sReader.ReadLine();
-            } catch (IOException)
-            {
-                sWriter.Close();
-                sReader.Close();
-                client.Close();
-            }
-            return message;
-        }
-    }
-    public static class JSON
-    {
-        public static dynamic ToObj<T>(string json) { return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json); }
+    public static class JSON {
+        public static dynamic? ToObj<T>(string json) { return json != null ? Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json) : null; }
         public static string ToJson(dynamic obj) { return Newtonsoft.Json.JsonConvert.SerializeObject(obj); }
-        public static bool LoadAndCreateConfig<T>(ref T obj, string path)
-        {
-            if (System.IO.File.Exists(path))
-            {
+        public static bool LoadAndCreateConfig<T>(ref T obj, string path) {
+            if (System.IO.File.Exists(path)) {
                 obj = KUR.JSON.ToObj<T>(System.IO.File.ReadAllText(path));
                 return true;
-            } else
-            {
-                System.IO.File.WriteAllText(path, KUR.JSON.ToJson(obj));
+            } else {
+                if (obj != null) System.IO.File.WriteAllText(path, KUR.JSON.ToJson(obj));
                 return false;
             };
         }
     }
-    public static class Encrypt
-    {
-        static public string CalculateMD5Hash(string input)
-        {
-            using (MD5 md5 = MD5.Create())
-            {
-                // 将字符串转换为字节数组
+    public static class Encrypt {
+        static public string CalculateMD5Hash(string input) {
+            using (MD5 md5 = MD5.Create()) {
                 byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-
-                // 计算MD5哈希值并将结果转换为十六进制字符串
                 StringBuilder sb = new StringBuilder();
-                foreach (byte b in md5.ComputeHash(inputBytes))
-                {
+                foreach (byte b in md5.ComputeHash(inputBytes)) {
                     sb.Append(b.ToString("x2"));
                 }
                 return sb.ToString();
             }
         }
     }
-    public class AesEncryption
-    {
-        private byte[] Key;
-        private byte[] IV;
-
-        public AesEncryption()
-        {
-            using (Aes myAes = Aes.Create())
-            {
-                Key = myAes.Key;
-                IV = myAes.IV;
-            }
-        }
-        public void SetKI(byte[] key, byte[] iv) { this.Key = key; this.IV = iv; }
-
-        public string Encrypt(string plainText)
-        {
-            byte[] encrypted;
-
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(plainText);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-            }
-
-            return Convert.ToBase64String(encrypted);
-        }
-
-        public string Decrypt(string cipherText)
-        {
-            string plaintext = null;
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
-
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
-                {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
-
-            return plaintext;
-        }
-    }
-
 }
 
---
+++ lib.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace kur {
+    using size_t = System.IntPtr;
+    static class Mem {
+        public unsafe class MemoryPool {//TODO
+
+        }
+        public static unsafe void reverse<_Itr>(_Itr* _Begin, _Itr* _End) where _Itr : unmanaged {
+            while (_Begin < _End) {
+                --_End;
+                _Itr _Tmp = *_Begin;
+                *_Begin = *_End;
+                *_End = _Tmp;
+                ++_Begin;
+            };
+        }
+        public static unsafe void* malloc(size_t _Size) {
+            return Marshal.AllocHGlobal(_Size).ToPointer();
+        }
+        public static unsafe void free(void* _Ptr) {
+            Marshal.FreeHGlobal((IntPtr)_Ptr);
+        }
+        public unsafe class Vector<T> where T : unmanaged, IComparable<T> {
+            T* chunk = null;
+            size_t _size = 0;
+            size_t _capacity = 0;
+            public void expand() {
+                if (chunk == null) {
+                    this.create(0x2);
+                    return;
+                }
+                T* tmp = this.chunk;
+                size_t esize = sizeof(T) * (_capacity << 1);
+                this.chunk = (T*)malloc(esize);
+                if (this.chunk == null) {
+                    this.chunk = tmp;
+                    throw new Exception("bad alloc!");
+                }
+                Buffer.MemoryCopy(tmp, this.chunk, esize.ToInt64(), _size.ToInt64() * sizeof(T));
+                this._capacity = esize >> 1;
+                free(tmp);
+            }
+            public void create(size_t isize) {
+                this._capacity = isize;
+                this.chunk = (T*)malloc(isize);
+            }
+            public void construct(size_t isize) {
+                if (isize != 0) create(isize);
+            }
+            public void push_back(T value) {
+                if (this._size >= this._capacity) expand();
+                *(this.chunk + _size) = value;
+                ++this._size;
+            }
+            public bool is_full() {
+                return (this._size + 1) == this._capacity;
+            }
+            public T* data() {
+                return this.chunk;
+            }
+            public void clear() {
+                this._size = 0;
+            }
+            public ref T at(size_t idx) {
+                if (idx >= this._size) throw new Exception("Vector<T> out of range !");
+                return ref this.chunk[idx];
+            }
+            public ref T last() {
+                if (this._size == 0) throw new Exception("Vector<T> out of range !");
+                return ref this.chunk[this._size - 1];
+            }
+            public T* begin() {
+                return this.chunk;
+            }
+            public T* end() {
+                return this.chunk + this._size;
+            }
+            public void pop_back() {
+                if (this._size == 0) throw new Exception("Vector<T> out of range !");
+                --this._size;
+            }
+            public T* erase(size_t index) {
+                if (index >= this._size || this._size == 0) { throw new Exception("Vector<T> out of range !"); };
+                size_t _pos = this._size - 1;
+                for (size_t i = index; i < _pos; ++i) chunk[i] = chunk[i + 1];
+                --_pos;
+                return chunk + index;
+            }
+            public size_t length() {
+                return this._size;
+            }
+            public size_t size() {
+                return this._size;
+            }
+            public size_t capacity() {
+                return this._capacity;
+            }
+            public void init(T val, size_t size = 0) {
+                size_t sz = this._size;
+                if (size != 0) sz = size;
+                for (size_t i = 0; i < sz; ++i) *(this.chunk + i) = val;
+            }
+            public T* find(ref T value) {
+                for (size_t i = 0; i < this._size; ++i) if (chunk[i].Equals(value)) return &chunk[i];
+                return end();
+            }
+            public T this[int i] {
+                get => at(i);
+                set => chunk[i] = value;
+            }
+            public Vector(size_t init_size = 0) {
+                if (init_size == 0) return;
+                this.construct(init_size);
+                this._size = init_size;
+            }
+            ~Vector() {
+                if (chunk != null) free(chunk);
+            }
+        }
+    }
+    namespace net {
+        public static class Method {
+            static public async Task Send(NetworkStream io, Encoding enc, string str) {
+                try {
+                    await io.WriteAsync(BitConverter.GetBytes(str.Length));
+                    await io.WriteAsync(enc.GetBytes(str));
+                } catch (IOException) {
+                    io.Close();
+                }
+            }
+            static public async Task<string?> Receive(NetworkStream io, Encoding enc) {
+                try {
+                    byte[] buf4 = new byte[4];
+                    int readlen = 0;
+                    while (readlen < 4) {
+                        int read = await io.ReadAsync(buf4, readlen, 4 - readlen);
+                        if (read == 0) {
+                            io.Close();
+                            return null;
+                        }
+                        readlen += read;
+                    }
+                    int msglen = BitConverter.ToInt32(buf4, 0);
+                    byte[] buffer = new byte[msglen];
+                    readlen = 0;
+                    while (readlen < msglen) {
+                        int read = await io.ReadAsync(buffer, readlen, msglen - readlen);
+                        if (read == 0) {
+                            io.Close();
+                            return null;
+                        }
+                        readlen += read;
+                    }
+                    return enc.GetString(buffer);
+                } catch (IOException) {
+                    io.Close();
+                };
+                return null;
+            }
+        }
+        public class Server {
+            public TcpListener listener;
+            public IPAddress ip_adress;
+            public List<Task>? client_tasks = null;
+            public int port = 0;
+            public bool continue_ = true;
+            public Encoding encoding = Encoding.ASCII;
+            public delegate Task<bool> Request(NetworkStream io);
+            Request request;
+            public Server(ref string ip, int port, Request req) {
+                if (ip.Length == 0) this.ip_adress = IPAddress.Any;
+                else this.ip_adress = IPAddress.Parse(ip);
+                this.port = port;
+                this.listener = new TcpListener(this.ip_adress, this.port);
+                this.request = req;
+            }
+            public void SetEncoding(Encoding enc) {
+                this.encoding = enc;
+            }
+            public void Start() {
+                listener.Start();
+                client_tasks = new List<Task>();
+                while (continue_) {
+                    client_tasks.Add(Handle(listener.AcceptTcpClient()));
+                    client_tasks.RemoveAll(t => t.IsCompleted || t.IsFaulted || t.IsCanceled);
+                };
+            }
+            public void Stop() {
+                this.continue_ = false;
+                listener.Stop();
+            }
+            public async Task Handle(TcpClient client) {
+                using NetworkStream iostr = client.GetStream();
+                try {
+                    while (await request(iostr)) ;
+                } finally {
+                    iostr.Close();
+                    client.Close();
+                }
+            }
+            ~Server() {
+                this.Stop();
+            }
+        }
+        public class Client {
+            public TcpClient client;
+            public IPAddress ip_adress;
+            public int port;
+            public Encoding encoding = Encoding.ASCII;
+            public Client(ref string ip, int port, Encoding enc) {
+                this.ip_adress = IPAddress.Parse(ip);
+                this.port = port;
+                this.client = new TcpClient();
+                this.encoding = enc;
+            }
+            public void Connect() {
+                client.Connect(this.ip_adress, this.port);
+            }
+            public void Close() {
+                client.Close();
+            }
+            public NetworkStream GetStream() {
+                return client.GetStream();
+            }
+            public async void SendAsync(string str) {
+                await Method.Send(client.GetStream(), this.encoding, str);
+            }
+            public async Task<string?> ReceiveAsync() {
+                string? str = await Method.Receive(client.GetStream(), this.encoding);
+                return str;
+            }
+            ~Client() {
+                client.Close();
+            }
+        }
+        public class HttpServer {
+            public int port;
+            HttpListener listener;
+            public bool continue_ = true;
+            public bool outlog = false;
+            HttpListenerContext? content = null;
+            public delegate bool Request(HttpListenerContext req);
+            Request request;
+            public HttpServer(int port, Request req) {
+                this.port = port;
+                this.listener = new HttpListener();
+                this.listener.Prefixes.Add($"http://+:{port}/");
+                this.request = req;
+            }
+            public void Start() {
+                listener.Start();
+            step:
+                if (!NetworkInterface.GetIsNetworkAvailable()) {
+                    Console.WriteLine("No network connection available.");
+                    Thread.Sleep(1000);
+                    goto step;
+                };
+                try {
+                    while (continue_ && request(this.content = listener.GetContext())) ;
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.ToString());
+                    if (this.content != null) {
+                        this.content.Response.Close();
+                        //this.Stop();
+                        if (outlog) {
+                            System.Diagnostics.Trace.WriteLine($"{DateTime.Now}:{ex}");
+                            System.Diagnostics.Trace.Flush();
+                        }
+                    }
+                }
+                goto step;
+            }
+            public void Stop() {
+                this.continue_ = false;
+                listener.Stop();
+            }
+        }
+    }
+}
